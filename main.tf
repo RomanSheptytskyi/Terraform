7a636f030e4b6cd4c89644549ac9e1e1201fd908
RomanSheptytskyi <roman@DESKTOP-TSGOK0S>
2024-03-27 18:42:02 +0200

    :0

---
+++ main.tf
@@ -0,0 +1,36 @@
+locals {
+  tag_name = var.use_locals ? "forum" : var.bucket_name  
+}
+
+module "s3" {
+  source = "./modules/s3"
+  bucket_name = "${data.aws_caller_identity.current.account_id}-my-tf-test-bucket-s3"
+  use_locals = true
+}
+
+module "table_authors" {
+  source = "./modules/dynamodb"
+  context = module.label.context
+  name = "authors"
+}
+
+module "table_courses" {
+  source = "./modules/dynamodb"
+  context = module.label.context
+  name = "courses"
+}
+
+module "lambdas" {
+  source = "./modules/lambda"
+  context = module.label.context
+  table_authors_name = module.table_authors.id
+  table_authors_arn = module.table_authors.arn
+  table_courses_name = module.table_courses.id
+  table_courses_arn = module.table_courses.arn
+}
+
+
+resource "aws_s3_bucket" "this" {
+  bucket = module.label.id
+  tags = module.label_s3.tags
+}
---
+++ modules/s3/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+    type = string
+    # default = "891377221357-my-tf-test-bucket"
+}
+
+variable "use_locals" {
+    type = bool
+    # default = "891377221357-my-tf-test-bucket"
+}
